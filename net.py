86772e8875f1af688ca90f78b63a63a3f8b75fe7
wgqsjtu <kiwi341@sjtu.edu.cn>
2021-11-18 20:09:16 +0800

    tmp

---
+++ net.py
@@ -0,0 +1,318 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class Net64x64(torch.nn.Module):
+    def __init__(self):
+        super(Net64x64, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 4, kernel_size=4, stride=4, padding=0, bias=True) # (1, 64, 64) -> (4, 16, 16)
+        self.NC_p1 = nn.Conv2d(2, 4, kernel_size=4, stride=4, padding=0, bias=True) # (2, 64, 64) -> (4, 16, 16)
+
+        self.NC_o2 = nn.Conv2d(4, 16, kernel_size=4, stride=4, padding=0, bias=True) # (4, 16, 16) -> (16, 4, 4)
+        self.NC_p2 = nn.Conv2d(4, 16, kernel_size=4, stride=4, padding=0, bias=True) # (4, 16, 16) -> (16, 4, 4)
+
+        self.NC3 = nn.Conv2d(32, 128, kernel_size=4, stride=4, padding=0, bias=True) # (32, 4, 4) -> (128, 1, 1)
+
+        self.FC1 = nn.Linear(129,32) # 129 -> 32
+        self.FC2 = nn.Linear(33,2) # 33 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        F_o = F.relu(self.NC_o2(F_o))
+        F_p = F.relu(self.NC_p2(F_p))
+
+        T = F.relu(self.NC3(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x = F.relu(self.FC1(torch.cat((T, qp), dim=1)))
+        x = F.relu(self.FC2(torch.cat((x, qp), dim=1)))
+        return x
+
+class Net32x32(torch.nn.Module):
+    def __init__(self):
+        super(Net32x32, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=4, stride=4, padding=0, bias=True) # (1, 32, 32) -> (8, 8, 8)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=4, stride=4, padding=0, bias=True) # (2, 32, 32) -> (8, 8, 8)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=4, stride=4, padding=0, bias=True) # (16, 8, 8) -> (64, 2, 2)
+        self.NC3 = nn.Conv2d(64, 128, kernel_size=4, stride=4, padding=0, bias=True) # (64, 2, 2) -> (128, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(129,32) # 129 -> 32
+        self.FC1_stage2 = nn.Linear(129,32) # 129 -> 32
+
+        self.FC2_stage1 = nn.Linear(33,2) # 33 -> 2
+        self.FC2_stage2 = nn.Linear(33,2) # 33 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+        T = F.relu(self.NC3(T))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+
+        return x1, x2
+
+class Net16x16(torch.nn.Module):
+    def __init__(self):
+        super(Net16x16, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=4, stride=4, padding=0, bias=True) # (1, 16, 16) -> (8, 4, 4)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=4, stride=4, padding=0, bias=True) # (2, 16, 16) -> (8, 4, 4)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=4, stride=4, padding=0, bias=True) # (16, 4, 4) -> (64, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(65,16) # 65 -> 16
+        self.FC1_stage2 = nn.Linear(65,16) # 65 -> 16
+
+        self.FC2_stage1 = nn.Linear(17,2) # 17 -> 2
+        self.FC2_stage2 = nn.Linear(17,2) # 17 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+        
+        return x1, x2
+
+class Net32x16(torch.nn.Module):
+    def __init__(self):
+        super(Net32x16, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(8, 4), stride=4, padding=0, bias=True) # (1, 32, 16) -> (8, 4, 4)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(8, 4), stride=4, padding=0, bias=True) # (2, 32, 16) -> (8, 4, 4)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=4, stride=4, padding=0, bias=True) # (16, 4, 4) -> (64, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(65,16) # 65 -> 16
+        self.FC1_stage2 = nn.Linear(65,16) # 65 -> 16
+
+        self.FC2_stage1 = nn.Linear(17,2) # 17 -> 2
+        self.FC2_stage2 = nn.Linear(17,2) # 17 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+        
+        return x1, x2
+
+class Net8x8(torch.nn.Module):
+    def __init__(self):
+        super(Net8x8, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=4, stride=4, padding=0, bias=True) # (1, 8, 8) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=4, stride=4, padding=0, bias=True) # (2, 8, 8) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(33,16) # 33 -> 16
+        self.FC1_stage2 = nn.Linear(33,16) # 33 -> 16
+
+        self.FC2_stage1 = nn.Linear(17,2) # 17 -> 2
+        self.FC2_stage2 = nn.Linear(17,2) # 17 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+        
+        return x1, x2
+
+class Net32x8(torch.nn.Module):
+    def __init__(self):
+        super(Net32x8, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(16, 4), stride=4, padding=0, bias=True) # (1, 32, 8) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(16, 4), stride=4, padding=0, bias=True) # (2, 32, 8) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(33,16) # 33 -> 16
+        self.FC1_stage2 = nn.Linear(33,16) # 33 -> 16
+
+        self.FC2_stage1 = nn.Linear(17,2) # 17 -> 2
+        self.FC2_stage2 = nn.Linear(17,2) # 17 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+        
+        return x1, x2
+
+class Net16x8(torch.nn.Module):
+    def __init__(self):
+        super(Net16x8, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(8, 4), stride=4, padding=0, bias=True) # (1, 16, 8) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(8, 4), stride=4, padding=0, bias=True) # (2, 16, 8) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1_stage1 = nn.Linear(33,16) # 33 -> 16
+        self.FC1_stage2 = nn.Linear(33,16) # 33 -> 16
+
+        self.FC2_stage1 = nn.Linear(17,2) # 17 -> 2
+        self.FC2_stage2 = nn.Linear(17,2) # 17 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x1 = F.relu(self.FC1_stage1(torch.cat((T, qp), dim=1)))
+        x1 = F.relu(self.FC2_stage1(torch.cat((x1, qp), dim=1)))
+
+        x2 = F.relu(self.FC1_stage2(torch.cat((T, qp), dim=1)))
+        x2 = F.relu(self.FC2_stage2(torch.cat((x2, qp), dim=1)))
+        
+        return x1, x2
+
+class Net32x4(torch.nn.Module):
+    def __init__(self):
+        super(Net32x4, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(16, 2), stride=4, padding=0, bias=True) # (1, 32, 4) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(16, 2), stride=4, padding=0, bias=True) # (2, 32, 4) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1 = nn.Linear(33,2) # 33 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x = F.relu(self.FC1(torch.cat((T, qp), dim=1)))
+        
+        return x
+
+class Net16x4(torch.nn.Module):
+    def __init__(self):
+        super(Net16x4, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(8, 2), stride=4, padding=0, bias=True) # (1, 16, 4) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(8, 2), stride=4, padding=0, bias=True) # (2, 16, 4) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1 = nn.Linear(33,2) # 33 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x = F.relu(self.FC1(torch.cat((T, qp), dim=1)))
+        
+        return x
+
+class Net8x4(torch.nn.Module):
+    def __init__(self):
+        super(Net8x4, self).__init__()
+
+        self.NC_o1 = nn.Conv2d(1, 8, kernel_size=(4, 2), stride=4, padding=0, bias=True) # (1, 8, 4) -> (8, 2, 2)
+        self.NC_p1 = nn.Conv2d(2, 8, kernel_size=(4, 2), stride=4, padding=0, bias=True) # (2, 8, 4) -> (8, 2, 2)
+
+        self.NC2 = nn.Conv2d(16, 64, kernel_size=2, stride=2, padding=0, bias=True) # (16, 2, 2) -> (32, 1, 1)
+
+        self.FC1 = nn.Linear(33,2) # 33 -> 2
+
+    def forward(self, org, pre, qp):
+
+        F_o = F.relu(self.NC_o1(org))
+        F_p = F.relu(self.NC_p1(torch.cat((org, pre), dim=1)))
+
+        T = F.relu(self.NC2(torch.cat((F_o, F_p), dim=1)))
+
+        T = T.squeeze(3).squeeze(2)
+        qp = qp / 64 - 0.5
+
+        x = F.relu(self.FC1(torch.cat((T, qp), dim=1)))
+        
+        return x
+
+if __name__ == '__main__':
+
+    model = Net64x64()
+    x = torch.ones(1, 1, 64, 64)
+    out = model(x, x, torch.ones(1, 1))
+    print (out)
--- train.py
+++ train.py
@@ -4,18 +4,46 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-from model import Net
+import net
 from dataset import DatasetFromFolder
 import time
 import os
 import numpy as np
+import argparse
 #import torchsnooper
 
+# Training settings
+parser = argparse.ArgumentParser(description='Example')
+parser.add_argument('--cu_size', type=str, default='32x32', help="cu size")
+parser.add_argument('--data_dir', type=str, default='/home/wgq/research/bs/dataset/32x32', help='data dir')
+parser.add_argument('--train_list', type=str, default='train_list.txt', help='train list file')
+parser.add_argument('--test_list', type=str, default='test_list.txt', help='test list file')
+parser.add_argument('--batch_size', type=int, default=32, help='training and testing batch size')
+parser.add_argument('--threads', type=int, default=8, help='number of threads for data loader to use')
+parser.add_argument('--nEpochs', type=int, default=50000, help='number of epochs')
+parser.add_argument('--output', default='model/', help='Location to save checkpoint models')
+parser.add_argument('--model', default='', help='pretrained base model')
+parser.add_argument('--pretrain_epoch', type=int, default=0, help='pretrain epoch')
+parser.add_argument('--lr', type=float, default=1e-4, help='learn rate')
+
+opt = parser.parse_args()
+
+cu_size = opt.cu_size
+data_dir = opt.data_dir
+train_list = opt.train_list
+test_list = opt.test_list
+batchSize = opt.batch_size
+threads = opt.threads
+lr = opt.lr
+output = opt.output
+nEpochs = opt.nEpochs
+pretrained_model = opt.model
+pretrain_epoch = opt.pretrain_epoch
+
 def checkpoint(epoch):
-    model_out_path = './model/model_epoch' + str(epoch) + '.pth'
+    model_out_path = output + '/model_epoch' + str(epoch) + '.pth'
     torch.save(model.state_dict(), model_out_path)
     print("Checkpoint saved to {}".format(model_out_path))
-
 
 def print_network(net):
     num_params = 0
@@ -51,7 +79,6 @@ def train(epoch):
         # print("===> Epoch[{}]({}/{}): Loss: {:.6f} || Timer: {:.4f} sec.".format(epoch, iteration, len(training_data_loader), loss.item(), (t3 - t2)))
     t1 = time.time()
     print("===> Epoch [{}] Complete: Loss: {:.6f} || Timer: {:.4f} sec.".format(epoch, epoch_loss / len(training_data_loader), (t1 - t0)))
-
 
 def test():
 
@@ -93,16 +120,6 @@ def test():
     print('划分预测准确度:', TN/(FP+TN), str(TN) + '/' + str(FP+TN))
     print('--------------------------------------------------')
 
-## 参数
-threads = 8 #数据加载线程
-data_dir = '/home/wgq/research/bs/dataset/32x32'
-train_list = 'train_list.txt'
-test_list = 'test_list.txt'
-batchSize = 32
-lr = 1e-4
-nEpochs = 50000
-pretrained = 1960
-model_path = './model/model_epoch1960.pth'
 
 print('===> Loading datasets')
 train_set = DatasetFromFolder(data_dir, train_list, 32, 32)
@@ -112,12 +129,33 @@ test_set = DatasetFromFolder(data_dir, test_list, 32, 32)
 testing_data_loader = DataLoader(dataset=test_set, num_workers=threads, batch_size=batchSize, shuffle=True)
 
 print('===> Building model ')
-model = Net()
+
+if cu_size == '64x64':
+    model = net.Net64x64()
+elif cu_size == '32x32':
+    model = net.Net32x32()
+elif cu_size == '16x16':
+    model = net.Net16x16()
+elif cu_size == '8x8':
+    model = net.Net8x8()
+elif cu_size == '32x16':
+    model = net.Net32x16()
+elif cu_size == '32x8':
+    model = net.Net32x8()
+elif cu_size == '32x4':
+    model = net.Net32x4()
+elif cu_size == '16x8':
+    model = net.Net16x8()
+elif cu_size == '16x4':
+    model = net.Net16x4()
+elif cu_size == '8x4':
+    model = net.Net8x4()
+
 criterion = nn.CrossEntropyLoss()
 
-if pretrained:
-    model.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
-    print('Pre-trained SR model is loaded.')
+if pretrained_model != '':
+    model.load_state_dict(torch.load(pretrained_model, map_location=lambda storage, loc: storage))
+    print('Pre-trained model is loaded.')
 
 model = model.cuda(0)
 criterion = criterion.cuda(0)
@@ -131,15 +169,15 @@ optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999), eps=1e-8)
 for epoch in range(1, nEpochs + 1):
 
     print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    train(epoch+pretrained)
+    train(epoch+pretrain_epoch)
 
-    if (epoch+pretrained)%10 == 0:
-        checkpoint(epoch+pretrained)
+    if (epoch+pretrain_epoch)%10 == 0:
+        checkpoint(epoch+pretrain_epoch)
         test()
 
 
     # learning rate is decayed by a factor of 10 every half of total epochs
-    if (epoch+pretrained) % 500 == 0:
+    if (epoch+pretrain_epoch) % 200 == 0:
         for param_group in optimizer.param_groups:
             param_group['lr'] /= 1.1
         print('Learning rate decay: lr={}'.format(optimizer.param_groups[0]['lr']))
